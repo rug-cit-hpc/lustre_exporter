b5d01a3606765de124d7e1cf54793e4cbea3dc12
Gabriele Iannetti <g.iannetti@gsi.de>
2023-04-24 08:50:13 +0200

    Update RPM build script

--- rpm/build.sh
+++ rpm/build.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
+
 set -e
+
+function do_checks {
+
+  info_msg="The build script must be executed from the projects base directory!"
+
+  if [ -z "$VERSION" ]; then
+    echo "ERROR: Build failed! VERSION file not found" >&2
+    echo "INFO: $info_msg"
+    exit 1
+  fi
+
+}
+
 export VERSION=$(cat VERSION)
-export promdir=prometheus-lustre-exporter-$VERSION
-export builddir=$HOME/rpmbuild
+export BUILD_DIR=$HOME/rpmbuild
+export PKG_DIR=prometheus-lustre-exporter-$VERSION
+
 make build
-#use VERSION File as Version
+
 sed -i "s/VERSION/$(cat VERSION)/" rpm/prometheus-lustre-exporter.spec
-mkdir -p $builddir/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-mkdir -p $builddir/SOURCES/$promdir/usr/bin
-mkdir -p $builddir/SOURCES/$promdir/usr/lib/systemd/system
-mkdir -p $builddir/SOURCES/$promdir/etc/sysconfig
-mkdir -p $builddir/SOURCES/$promdir/etc/sudoers.d
-mkdir -p $builddir/SOURCES/$promdir/var/log/prometheus-lustre-exporter
-cp rpm/prometheus-lustre-exporter.spec $builddir/SPECS/
-cp systemd/prometheus-lustre-exporter.service $builddir/SOURCES/$promdir/usr/lib/systemd/system/
-cp systemd/prometheus-lustre-exporter.options $builddir/SOURCES/$promdir/etc/sysconfig/
-cp sudoers/prometheus-lustre-exporter $builddir/SOURCES/$promdir/etc/sudoers.d/
-cp lustre_exporter $builddir/SOURCES/$promdir/usr/bin/
-cd $builddir/SOURCES
-tar -czvf $promdir.tar.gz $promdir
-cd $builddir
-echo build dir is $builddir
-ls -la $builddir/SOURCES
-rpmbuild -ba  $builddir/SPECS/prometheus-lustre-exporter.spec
+mkdir -p $BUILD_DIR/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+mkdir -p $BUILD_DIR/SOURCES/$PKG_DIR/usr/bin
+mkdir -p $BUILD_DIR/SOURCES/$PKG_DIR/usr/lib/systemd/system
+mkdir -p $BUILD_DIR/SOURCES/$PKG_DIR/etc/sysconfig
+mkdir -p $BUILD_DIR/SOURCES/$PKG_DIR/etc/sudoers.d
+mkdir -p $BUILD_DIR/SOURCES/$PKG_DIR/var/log/prometheus-lustre-exporter
+cp rpm/prometheus-lustre-exporter.spec $BUILD_DIR/SPECS/
+cp systemd/prometheus-lustre-exporter.service $BUILD_DIR/SOURCES/$PKG_DIR/usr/lib/systemd/system/
+cp systemd/prometheus-lustre-exporter.options $BUILD_DIR/SOURCES/$PKG_DIR/etc/sysconfig/
+cp sudoers/prometheus-lustre-exporter $BUILD_DIR/SOURCES/$PKG_DIR/etc/sudoers.d/
+cp lustre_exporter $BUILD_DIR/SOURCES/$PKG_DIR/usr/bin/
+cd $BUILD_DIR/SOURCES
+tar -czvf $PKG_DIR.tar.gz $PKG_DIR
+cd $BUILD_DIR
+echo build dir is $BUILD_DIR
+ls -la $BUILD_DIR/SOURCES
+rpmbuild -ba  $BUILD_DIR/SPECS/prometheus-lustre-exporter.spec
 
