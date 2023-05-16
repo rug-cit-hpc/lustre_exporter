aed58a94686919d922828f4645f331946e0ac5dd
Gabriele Iannetti <g.iannetti@gsi.de>
2023-05-16 08:50:20 +0200

    Change lnet metrics to 2.12.5 paths

--- sources/procsys.go
+++ sources/procsys.go
@@ -92,7 +92,7 @@ func (s *lustreProcSysSource) generateLNETTemplates(filter string) {
 
 func newLustreProcSysSource() LustreSource {
 	var l lustreProcSysSource
-	l.basePath = filepath.Join(ProcLocation, "sys")
+	l.basePath = filepath.Join(SysLocation, "kernel/debug")
 	if LnetEnabled != disabled {
 		l.generateLNETTemplates(LnetEnabled)
 	}
