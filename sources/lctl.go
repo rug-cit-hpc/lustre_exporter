e6f2daf9176b1708e2916213acde0509109d826b
Gabriele Iannetti <g.iannetti@gsi.de>
2023-11-16 11:19:25 +0100

    Update licence

--- sources/lctl.go
+++ sources/lctl.go
@@ -1,16 +1,10 @@
-// (C) Copyright 2021 Gabriele Iannetti <g.iannetti@gsi.de>
-// (C) Copyright 2017 Hewlett Packard Enterprise Development LP
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// -*- coding: utf-8 -*-
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+// © Copyright 2023 GSI Helmholtzzentrum für Schwerionenforschung
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// This software is distributed under
+// the terms of the GNU General Public Licence version 3 (GPL Version 3),
+// copied verbatim in the file "LICENCE".
 
 package sources
 
